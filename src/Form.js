f91ca04c6f34aa06fe0b6163152b3fe9a6c660be
jwebs22 <jenny.webster@codeimmersives.com>
2023-02-23 15:59:38 -0500

    Code not getting errors, but not displaying app in browser

---
+++ src/Form.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+
+    // constructors are initialized when Form is created (instantiated)
+    // we set the initial state of our form and save it to state.
+    constructor(props){
+        super(props); //inheriting props from Component
+
+        // set the initial state of the Form
+        this.initialState = {
+            title: '',
+            actors: '',
+            plot: '',
+            genre: '',
+            imdbRating: '',
+            director: '',
+            year: '',
+            dateAdded: ''
+        };
+
+        //set our initial state to state.
+        this.state = this.initialState;
+    }
+
+    //name? or title?
+    handleChange = event => {
+        //get name, value out of target (element that changed)
+        const { name, value } = event.target;
+
+        //update our state 
+        this.setState({
+            [name]: value
+        });
+    }
+
+    onFormSubmit = (event) => {
+        //standard code for onSubmits 
+        event.preventDefault();
+        
+        //set the current state of our form to the handle submit
+        this.props.addMovie(this.state);
+
+        //clear inputs by setting form to inital state
+        this.setState(this.initialState);
+    
+    }
+
+    render() {
+        //hook in data from state 
+        const { title, actors, plot, genre, imdbRating, director, year, dateAdded } = this.state;
+        return (
+            <form onSubmit={this.onFormSubmit}>
+                <label htmlFor="title">Title :</label><br/>
+                <input 
+                    type="text" 
+                    id="title" 
+                    name="title"
+                    value={title}
+                    onChange={this.handleChange}
+                    /><br/>
+                <label htmlFor="actors">Actors :</label><br/>
+                <input 
+                    type="text" 
+                    id="actors" 
+                    name="actors"
+                    value={actors} 
+                    onChange={this.handleChange}
+                    /><br/>
+                <label htmlFor="plot">Plot :</label><br/>
+                <input 
+                    type="text" 
+                    id="plot" 
+                    name="plot"
+                    value={plot}
+                    onChange={this.handleChange}
+                    /><br/>
+                <label htmlFor="genre">Genre :</label><br/>
+                <input 
+                    type="genre" 
+                    id="genre" 
+                    name="genre"
+                    value={genre}
+                    onChange={this.handleChange}
+                    /><br/>
+                <label htmlFor="imdbRating">imdb Rating :</label><br/>
+                <input 
+                    type="imdbRating" 
+                    id="imdbRating" 
+                    name="imdbRating"
+                    value={imdbRating}
+                    onChange={this.handleChange}
+                    /><br/>
+                <label htmlFor="director">Director :</label><br/>
+                <input 
+                    type="director" 
+                    id="director" 
+                    name="director"
+                    value={director}
+                    onChange={this.handleChange}
+                    /><br/>
+                <label htmlFor="year">Year :</label><br/>
+                <input 
+                    type="year" 
+                    id="year" 
+                    name="year"
+                    value={year}
+                    onChange={this.handleChange}
+                    /><br/>    
+                <label htmlFor="dateAdded">Date Added :</label><br/>
+                <input 
+                    type="dateAdded" 
+                    id="dateAdded" 
+                    name="dateAdded"
+                    value={dateAdded}
+                    onChange={this.handleChange}
+                    />
+                <button type="submit">
+                    Submit
+                </button>
+            </form>
+        );
+    }
+}
+
+export default Form
---
+++ src/Table.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+
+ 
+//Table Header
+ const TableHeader = () => {
+     return (
+         <thead>
+               <tr>
+                 <th>Title</th>
+                 <th>Actors</th>
+                 <th>Plot</th>
+                 <th>Genre</th>
+                 <th>imdb Rating</th>
+                 <th>Director</th>
+                 <th>Year</th>
+                 <th>Date Added</th>
+               </tr>
+             </thead>
+     );
+ }
+ 
+//Table Body
+ const TableBody = (props) => {
+ 
+//Build Rows, map over each row and wrap it in an html table
+//Add onClick listener to delete the movie. 
+
+const rows = props.data.map((row, index) => {
+return (
+    <tr key={index}>
+    <td>{row.title}</td>
+    <td>{row.actors}</td>
+    <td>{row.plot}</td>
+    <td>{row.genre}</td>
+    <td>{row.imdbRating}</td>
+    <td>{row.director}</td>
+    <td>{row.year}</td>
+    <td>{row.dateAdded}</td>
+    <td><button onClick={() => props.removeMovie(index)}> Delete</button></td>
+    </tr>
+)
+})
+//Return rows wrapped in the table body
+return <tbody>{rows}</tbody>
+}
+
+
+//Table component
+ class Table extends Component {
+    render() {
+        //read props passed in from App.js
+        const { 
+            movieData, 
+            removeMovie
+        } = this.props;
+ 
+        return ( 
+           <table>
+            <TableHeader/>
+            <TableBody 
+               data={movieData} 
+               removeMovie={removeMovie}
+            />
+           </table>
+        )
+    }
+}
+ 
+ export default Table
